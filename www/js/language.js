bb896219b74fcca3f75baef01c9f1d3841552c26
piucommunication <piumarketing@gmail.com>
2018-06-05 15:09:54 +0200

    Implentate lingue in language.js

--- www/js/language.js
+++ www/js/language.js
@@ -1,6 +1,113 @@
-
-$( document ).ready(function() {
+var MessageTranslation = function(successCallback, errorCallback) {
  
+	this.setItalian = function() {
+		// pagina login
+	    this.logiBtLabel = "Entra";
+	    this.registerBtLabel = "Registrati";
+	    this.fbBtLabel = "Login con Facebook";
+	    this.gplusBtLabel = "Login con Google+";
+	    this.newRegisterBtLabel = "Nuova registrazione";
+	    
+	    // home
+
+	    // lista catalogo
+	    this.titCatalogo= "Catalogo";
+
+	    //dettaglio catalogo
+
+	    //katoimer
+
+	    //MA.MO.T.
+
+	    //contatti
+	}
+
+	this.setEnglish = function() {
+		// pagina login
+	    this.logiBtLabel = "Enter";
+	    this.registerBtLabel = "Register";
+	    this.fbBtLabel = "Login with Facebook";
+	    this.gplusBtLabel = "Login with Google+";
+	    this.newRegisterBtLabel = "New registration";
+	    
+	    // home
+
+	    // lista catalogo
+	    this.titCatalogo= "Catalogue";
+
+	    //dettaglio catalogo
+
+	    //katoimer
+
+	    //MA.MO.T.
+
+	    //contatti
+	}
+
+	this.setDeutch = function() {
+		// pagina login
+	    this.logiBtLabel = "Anmeldung";
+	    this.registerBtLabel = "Register";
+	    this.fbBtLabel = "Login with Facebook";
+	    this.gplusBtLabel = "Login with Google+";
+	    this.newRegisterBtLabel = "New registration";
+	    
+	    // home
+
+	    // lista catalogo
+	    this.titCatalogo= "Catalogo DE";
+
+	    //dettaglio catalogo
+
+	    //katoimer
+
+	    //MA.MO.T.
+
+	    //contatti
+	}
 
 
-});
+	this.setSpanish = function() {
+	    this.logiBtLabel = "Entrar";
+	    this.registerBtLabel= "Registrati";
+	    this.fbBtLabel = "Login with Facebook";
+	    this.gplusBtLabel = "Login with Google+";
+	    this.newRegisterBtLabel= "Nuova registrazione";
+	    
+	    // home
+
+	    // lista catalogo
+	    this.titCatalogo= "Catalogo ES";
+
+	    //dettaglio catalogo
+
+	    //katoimer
+
+	    //MA.MO.T.
+
+	    //contatti
+	}
+
+
+	this.setFrench = function() {
+	    this.logiBtLabel = "Entrèe";
+	    this.registerBtLabel= "Registre";
+	    this.fbBtLabel = "Login with Facebook";
+	    this.gplusBtLabel = "Login with Google+";
+	    this.newRegisterBtLabel= "Nuova registrazione";
+	    
+	    // home
+
+	    // lista catalogo
+	    this.titCatalogo= "Catalogo FR";
+
+	    //dettaglio catalogo
+
+	    //katoimer
+
+	    //MA.MO.T.
+
+	    //contatti
+	}
+
+}
