dd7a42271c05f091eabd06b507a97129bd82b053
piucommunication <piumarketing@gmail.com>
2018-06-14 11:15:54 +0200

    Versione Test interno pubblicata

--- www/js/main.js
+++ www/js/main.js
@@ -1117,7 +1117,7 @@ var app = {
     checkGooglePlugin: function() {
         window.plugins.googleplus.login(
         {
-          'webClientId': 'client id of the web app/server side', // optional clientId of your Web application from Credentials settings of your project - On Android, this MUST be included to get an idToken. On iOS, it is not required.
+          'webClientId': '428188435311-vhk6b8hhtr9lhmiobs9d9cil5nlkh7l2.apps.googleusercontent.com', // optional clientId of your Web application from Credentials settings of your project - On Android, this MUST be included to get an idToken. On iOS, it is not required.
           'offline': true // optional, but requires the webClientId - if set to true the plugin will also return a serverAuthCode, which can be used to grant offline access to a non-Google server
         },
         function (obj) {
